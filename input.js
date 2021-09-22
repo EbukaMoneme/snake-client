200a06a312e1016737835f0fae9beb4a0b6dd058
EbukaMoneme <camoneme@gmail.com>
2021-09-22 23:38:01 +0000

    Created a connection and coded input to control snake

--- input.js
+++ input.js
@@ -1,12 +1,28 @@
+// Stores the active TCP connection object.
+let connection;
+
 const handleUserInput = () => {
   process.stdin.on('data', (key) => {
     if (key === '\u0003') {
       process.exit();
     }
+    if (key === '\w') {
+      connection.write('Move: up');
+    }
+    if (key === '\a') {
+      connection.write('Move: left');
+    }
+    if (key === '\s') {
+      connection.write('Move: down');
+    }
+    if (key === '\d') {
+      connection.write('Move: right');
+    }
   });
 };
 
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
