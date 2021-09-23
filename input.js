816ac855b2a2622440f72572990caf4f8784af1d
EbukaMoneme <camoneme@gmail.com>
2021-09-23 00:39:02 +0000

    Created a constants file for sensitive information

--- input.js
+++ input.js
@@ -1,24 +1,36 @@
 // Stores the active TCP connection object.
 let connection;
 
-const handleUserInput = () => {
-  process.stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-    if (key === '\w') {
-      connection.write('Move: up');
-    }
-    if (key === '\a') {
-      connection.write('Move: left');
-    }
-    if (key === '\s') {
-      connection.write('Move: down');
-    }
-    if (key === '\d') {
-      connection.write('Move: right');
-    }
-  });
+const handleUserInput = (key) => {
+  let delay = 20;
+  // const stdin = process.stdin;
+  if (key === '\u0003') {
+    process.exit();
+  }
+  if (key === 'w') {
+    setTimeout(() => connection.write('Move: up'), delay);
+  }
+  if (key === 'a') {
+    setTimeout(() => connection.write('Move: left'), delay);
+  }
+  if (key === 's') {
+    setTimeout(() => connection.write('Move: down'), delay);
+  }
+  if (key === 'd') {
+    setTimeout(() => connection.write('Move: right'), delay);
+  }
+  if (key === 'j') {
+    setTimeout(() => connection.write('Say: BYE'), delay);
+  }
+  if (key === 'k') {
+    setTimeout(() => connection.write('Say: SUP'), delay);
+  }
+  if (key === 'l') {
+    setTimeout(() => connection.write('Say: RUN'), delay);
+  }
+  if (key === 'i') {
+    setTimeout(() => connection.write('Say: RUN AJ'), delay);
+  }
 };
 
 const setupInput = (conn) => {
