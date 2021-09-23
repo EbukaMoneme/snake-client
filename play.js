522cc6b5f290a5320693b32718404307241b821f
EbukaMoneme <camoneme@gmail.com>
2021-09-23 00:45:03 +0000

    Updates

--- play.js
+++ play.js
@@ -2,9 +2,6 @@ const net = require("net");
 const connect = require('./client');
 const setupInput = require('./input');
 
-
-
 console.log("Connecting ...");
-// connect();
 setupInput(connect());
 
